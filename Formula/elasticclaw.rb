c4bf5f1ab5013519b1dab7454c31b29835c1a5f5
Marc Campbell <marc.e.campbell@gmail.com>
2026-04-13 20:38:48 +0000

    Initial tap with elasticclaw formula

---
+++ Formula/elasticclaw.rb
@@ -0,0 +1,26 @@
+class Elasticclaw < Formula
+  desc "Provision and manage AI agent claws from the command line"
+  homepage "https://github.com/elasticclaw/elasticclaw"
+  version "0.1.0"
+  license "Apache-2.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/elasticclaw/elasticclaw/releases/download/v#{version}/elasticclaw-darwin-arm64"
+      sha256 "PLACEHOLDER_ARM64_SHA256"
+    end
+    on_intel do
+      url "https://github.com/elasticclaw/elasticclaw/releases/download/v#{version}/elasticclaw-darwin-amd64"
+      sha256 "PLACEHOLDER_AMD64_SHA256"
+    end
+  end
+
+  def install
+    bin_name = Hardware::CPU.arm? ? "elasticclaw-darwin-arm64" : "elasticclaw-darwin-amd64"
+    bin.install bin_name => "elasticclaw"
+  end
+
+  test do
+    assert_match "elasticclaw", shell_output("#{bin}/elasticclaw version 2>&1")
+  end
+end
---
+++ README.md
@@ -0,0 +1,25 @@
+# homebrew-elasticclaw
+
+Homebrew tap for [ElasticClaw](https://github.com/elasticclaw/elasticclaw).
+
+## Install
+
+```bash
+brew tap elasticclaw/elasticclaw
+brew install elasticclaw
+```
+
+## Upgrade
+
+```bash
+brew upgrade elasticclaw
+```
+
+## Usage
+
+```bash
+elasticclaw login https://your-hub.example.com --token your-token
+elasticclaw list
+elasticclaw create --name my-claw --template my-template
+elasticclaw chat my-claw
+```
